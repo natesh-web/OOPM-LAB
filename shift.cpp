1751e58688e55629c582fa5bdf1446aa1199c598
Natesh Talwane <nateshtalwane@gmail.com>
2026-09-16 15:14:32 +0530

    Update shift.cpp
    shifts zeros to the right side of the array

--- shift.cpp
+++ shift.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+   int n;
+   cout << "Enter size of array: ";
+   cin >> n;
+   int a[100];
+   cout << "Enter array elements: ";
+   for (int i = 0; i < n; i++) {
+       cin >> a[i];
+   }
+   // Move non-zero elements to the front
+   int j = 0;
+   for (int i = 0; i < n; i++) {
+       if (a[i] != 0) {
+           a[j] = a[i];
+           j++;
+       }
+   }
+  
+   // Fill remaining positions with zeros
+   while (j < n) { 
+     a[j] = 0; 
+     j++;
+   }
+     cout << "Array after shifting zeros to right: ";
+     for (int i = 0; i < n; i++) {
+     cout << a[i] << " ";
+     }
+     return 0;
+  } 
